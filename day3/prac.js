b72d744ea9c9cad7aa83d2f57de9fb97cd4626f2
Madhvendra Singh <smadhvendra20@gmail.com>
2024-10-15 15:32:26 +0530

    IIFE, asynchronous and anonymous functions, callbacks, callback hell and promises

---
+++ day3/prac.js
@@ -0,0 +1,53 @@
+// `setTimeout`
+// Immediately Invoked Function Expression (IIFE)
+// storing fucntions in variables
+// passing functions as arguments can only be done as callbacks which is just a reference to the function and is typed
+// without the parentheses, if you type the parenthese with the function name as an argument, it will be invoked immediately
+// the function will be invoked immediately and the return value will be passed as an argument
+// the function constructor is a function that creates a new function
+// arrow functions
+
+function roll(num, delay, nextRoll) {
+    setTimeout(
+        ()=> {
+            console.log("roll no : " + num);
+        }, delay
+    );
+    if (nextRoll) nextRoll();
+}
+
+// introduction to callback hell
+roll(1, 1000, () => {
+    roll(2, 2000, () => {
+        roll(3, 3000, () => {
+            roll(4, 4000)
+        });
+    });
+});
+
+// intor to promises
+const promise = new Promise(function (resolve, reject) {
+    console.log('My first promise');
+    resolve('resolved');
+    reject('rejected');
+});
+
+promise.then(console.log).catch(console.log);
+
+doing the previous rollNo thing with promises
+function getRoll(num, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("roll no :", num);
+            resolve();
+        }, delay);
+    });
+}
+
+getRoll(1, 1000).then(() => {
+    getRoll(2, 2000).then(() => {
+        getRoll(3, 3000).then(() => {
+            getRoll(4, 4000);
+        });
+    });
+});
