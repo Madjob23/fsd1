985b1824d0748ac6bae2d57608bbad9d6c2e4999
Madhvendra Singh <smadhvendra20@gmail.com>
2024-11-08 12:48:53 +0530

    Validate login

--- test-fsd-ce/script.js
+++ test-fsd-ce/script.js
@@ -1,27 +1,23 @@
-function validateLogin(username, password) {
-    if (username === "" || password === "") {
-        return "Fields cannon be empty!";
-    }
-    if (username === validUsername) {
-        return true;
-    } else {
-        return "Incorrect username";
-    }
-    if (password === validPassword) {
-        return true;
-    } else {
-        return "Incorrect password";
-    }
-}
 
-// Example usage:
-const usernameInput = document.getElementById("username").value;
-const passwordInput = document.getElementById("password").value;
 const validUsername = "admin";
 const validPassword = "password123";
+const submitButton = document.getElementById("submit");
 
-if (validateLogin(usernameInput, passwordInput)) {
-    console.log("Login successful!");
-} else {
-    console.log("Invalid username or password.");
+function validateLogin(username, password) {
+    if (username === "" || password === "") {
+        return "Fields cannot be empty!";
+    }
+    if (username === validUsername && password === validPassword) {
+        return true;
+    } else {
+        return "Incorrect credentials";
+    }
 }
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const usernameInput = document.getElementById("username").value;
+    const passwordInput = document.getElementById("password").value;
+    const result = validateLogin(usernameInput, passwordInput);
+    
+})
