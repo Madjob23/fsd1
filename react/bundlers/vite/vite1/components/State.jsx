bffea0cb82db5f138c102d92a80b8c157fb50681
Madhvendra Singh <smadhvendra20@gmail.com>
2024-11-22 12:19:55 +0530

    Chnage h1 text and color the right way
    - gave h1 inline style(jsx syntax) and used the hook variable
      to change both text and color.

--- react/bundlers/vite/vite1/components/State.jsx
+++ react/bundlers/vite/vite1/components/State.jsx
@@ -14,22 +14,18 @@ function State() {
       </button>
       <p>{count}</p>
       <br />
-      <h1>My facoritecolor is {headingColor}</h1>
+      <h1 style={{color:headingColor}}>My facoritecolor is {headingColor}</h1>
         <button onClick={()=>{
             setHeadingColor("red");
-            h1.style.color = "red";
             }}>Red</button>
         <button onClick={()=>{
             setHeadingColor("green");
-            h1.style.color = "green";
             }}>Green</button>
         <button onClick={()=>{
             setHeadingColor("blue");
-            h1.style.color = "blue";
             }}>Blue</button>
         <button onClick={()=>{
             setHeadingColor("yellow");
-            h1.style.color = "yellow";
             }
         }>Yellow</button>
     </div>
