422d8b48378239be7f91377ae178918a3f09a4b1
Madhvendra Singh <smadhvendra20@gmail.com>
2024-11-22 12:18:47 +0530

    Change h1 color my way
    - useState to change color text
    - but used vanilla javascript to change text color through css

--- react/bundlers/vite/vite1/components/State.jsx
+++ react/bundlers/vite/vite1/components/State.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 
 function State() {
     const [count, setCount] = useState(0);
+    const [headingColor, setHeadingColor] = useState("blue");
+    const h1 = document.querySelector("h1");
   return (
     <div>
       <button onClick={() => {setCount(count + 1)}}>
@@ -11,6 +13,25 @@ function State() {
         Ghatao
       </button>
       <p>{count}</p>
+      <br />
+      <h1>My facoritecolor is {headingColor}</h1>
+        <button onClick={()=>{
+            setHeadingColor("red");
+            h1.style.color = "red";
+            }}>Red</button>
+        <button onClick={()=>{
+            setHeadingColor("green");
+            h1.style.color = "green";
+            }}>Green</button>
+        <button onClick={()=>{
+            setHeadingColor("blue");
+            h1.style.color = "blue";
+            }}>Blue</button>
+        <button onClick={()=>{
+            setHeadingColor("yellow");
+            h1.style.color = "yellow";
+            }
+        }>Yellow</button>
     </div>
   )
 }
